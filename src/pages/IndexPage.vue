5130710fa88b21f514fe86cf253350bfbb4a604d
ncikovic <ncikovic@veleri.hr>
2024-12-05 10:40:21 +0100

    Provjera ishoda 1 i 2 - natalia

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="index-page">
+    <h1>Provjera ishoda 1 i 2</h1>
+    <!-- Ostali sadržaj komponente -->
+  </div>
+</template>
+
+<script setup>
+//
+</script>
+
+<style scoped>
+/* Stilovi za ovu komponentu */
+.index-page {
+  padding: 20px;
+}
+
+h1 {
+  font-size: 30px;
+  color: #333;
+}
+</style>
